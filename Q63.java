d600fb2fbf3f911bc4379e69a58487bb41fe5f68
mailjbpatil3796 <65150521+mailjbpatil3796@users.noreply.github.com>
2020-10-06 23:32:25 +0530

    Add files via upload

---
+++ Q63.java
@@ -0,0 +1,29 @@
+public class Q63 {
+
+
+	  public static void main(String[] args) {
+	    CountCharacters cc = new CountCharacters();
+	    cc.countChars("I am  a good boy");
+	  }
+} 
+	  public void countChars(String str){
+	    char[] strArr;
+	    do{
+	      strArr = str.toCharArray();
+	      char ch = strArr[0];
+	      int count = 1;
+	      for(int i = 1; i < strArr.length; i++){
+	        if(ch == strArr[i]){
+	          count++;
+	        }
+	      }
+	      // We don't need to count spaces
+	      if(((ch != ' ') && (ch != '\t'))){
+	        System.out.println(ch + " - " + count);
+	      }
+	      // replace all occurrence of the character 
+	      // which is already iterated and counted
+	      str = str.replace(""+ch, "");
+	    }while(strArr.length > 1);    
+	  }
+	
